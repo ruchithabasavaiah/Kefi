44f3b568603ef6da981468f127dd7267f99bce77
Ruchitha Basavaiah <ruchithabasavaiah@gmail.com>
2026-03-06 14:59:03 -0500

    Fix port binding

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
